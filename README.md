225a64688cc47a534d2eee5ab585cb2f6e7b9a26
SandrineDayma <sandrine.dayma@live.fr>
2023-07-05 14:17:07 +0200

    modification script js et ajout readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mon-site-cv
+
+## Bienvenue sur mon site CV !
+
+Cette première approche a pour but de montrer ma capacité à gérer des éléments en responsive.
+
+Vous pourrez y découvrir un *menu burger* implémenté grâce au super tuto de Ben BK que vous pourrez visionner ici  👉 https://www.youtube.com/watch?v=bUzXug3ep94
+
+Ainsi qu'une page d'accueil qui s'adapte à la taille de l'écran grace à *Flexbox*.
+
+Ce site est construction, la suite arrivera prochainement...
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let toggle = document.querySelector('.toggle'); // on récupère la class toggle
-let body = document.querySelector('body'); // on récupère la balise body
+let nav = document.querySelector('nav'); // on récupère la balise nav
 
-// quand l'utilisateur va cliquer sur la toggle on va lancer une fonction qui va nous permettre de rajouter une class open sur le body 
+// quand l'utilisateur va cliquer sur la toggle on va lancer une fonction qui va nous permettre de rajouter une class open sur la nav 
 // on saura que le menu est ouvert grace à la présence de cette classe open
 toggle.addEventListener ('click',function () {
-        body.classList.toggle('open');
+        nav.classList.toggle('open');
     }
 )
